b074b3c0260340c2f0b6014a3d38bc4104007e10
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-15 11:57:08 +0100

    snack 2

---
+++ src/useDate.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+const useDate = () => {
+
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+
+  })
+
+
+
+
+  return date
+}
+
+export default useDate
